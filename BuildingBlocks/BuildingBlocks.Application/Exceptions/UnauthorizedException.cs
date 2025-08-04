1a1f9baaed2e32aeb551442a677e33b36fafae37
AsNott2000 <nurullah.coskun@outlook.com>
2025-08-04 10:39:06 +0300

    unauthorized renamed

--- BuildingBlocks/BuildingBlocks.Application/Exceptions/UnauthorizedException.cs
+++ BuildingBlocks/BuildingBlocks.Application/Exceptions/UnauthorizedException.cs
@@ -1,7 +1,7 @@
 using System;
 namespace BuildingBlocks.Application.Exceptions;
 
-public class UnatuthorizedException(string error) : Exception
+public class UnauthorizedException(string error) : Exception
 {
     public string Error { get; } = error;
 }
