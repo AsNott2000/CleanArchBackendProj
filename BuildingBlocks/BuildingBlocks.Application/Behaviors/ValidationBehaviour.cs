23f508f8d97d4255b5325ea7912493971d266517
AsNott2000 <nurullah.coskun@outlook.com>
2025-08-06 10:15:52 +0300

    BuildingBlocks.Application Behaviours codded

---
+++ BuildingBlocks/BuildingBlocks.Application/Behaviors/ValidationBehaviour.cs
@@ -0,0 +1,40 @@
+using System;
+using FluentValidation;
+using MediatR;
+using FluentValidation.Results;
+using Humanizer;
+using BuildingBlocks.Application.Exceptions;
+
+
+namespace BuildingBlocks.Application.Behaviors;
+
+public class ValidationBehaviour<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken = default)
+    {
+        if (validators.Any())
+        {
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationResult = await validators.First().ValidateAsync(context, cancellationToken);
+
+            if (!validationResult.IsValid)
+            {
+                var failures = Serialize(validationResult.Errors);
+                throw new BadRequestException(Resources.Messages.BadRequest, failures);
+            }
+        }
+        return await next();
+    }
+    private static Dictionary<string, string[]> Serialize(IEnumerable<ValidationFailure> failures)
+    {
+        var camelCaseFailures = failures
+            .GroupBy(failure => failure.PropertyName.Camelize())
+            .ToDictionary(
+                group => group.Key,
+                group => group.Select(failures => failures.ErrorMessage).ToArray()
+            );
+        return camelCaseFailures;
+    }
+}
